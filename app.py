7e33e7f76b8457612b42c51775fd8a44603ba395
rohinielora <70495118+rohinielora@users.noreply.github.com>
2024-04-21 13:09:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
+from flask import Flask, render_template
+from climate_manager.controllers.overview import overview_view
+from climate_manager.controllers.impact import impact_view
 import sqlite3
-from flask import Flask, render_template, g
-from climate_manager.weather.controller import location_view, history_view
 
 app = Flask(__name__)
 
 def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect('climate.db')
-    return g.db
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('data.db')
+    return db
 
 @app.teardown_appcontext
-def close_db(exception=None):
-    db = g.pop('db', None)
+def close_connection(exception):
+    db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("base.html")
 
-@app.route('/location')
-def location():
-    return location_view()
+@app.route('/overview')
+def overview():
+    return overview_view(get_db())
 
-@app.route('/history')
-def history():
-    return history_view()
+@app.route('/impact')
+def impact():
+    return impact_view(get_db())
 
 if __name__ == '__main__':
     app.run(debug=True)
