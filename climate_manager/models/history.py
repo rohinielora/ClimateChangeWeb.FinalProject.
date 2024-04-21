73f514b61dc8da2bf306e9154f7251e266a6c5a5
rohinielora <70495118+rohinielora@users.noreply.github.com>
2024-04-21 12:13:44 -0400

    Create history.py

---
+++ climate_manager/models/history.py
@@ -0,0 +1,6 @@
+class History:
+    def __init__(self, location, date, weather_description, temperature):
+        self.location = location
+        self.date = date
+        self.weather_description = weather_description
+        self.temperature = temperature
