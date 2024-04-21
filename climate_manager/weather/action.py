08ab20941653fe4e19834ce592644cb5ecbce899
rohinielora <70495118+rohinielora@users.noreply.github.com>
2024-04-21 13:13:46 -0400

    Update and rename action.py to base.html

--- climate_manager/weather/action.py
+++
@@ -1,8 +0,0 @@
-from climate_manager.models.location import Location
-from climate_manager.models.history import History
-
-def create_location_profile(name, longitude, latitude, region, country):
-    return Location(name, longitude, latitude, region, country)
-
-def create_weather_history(location, date, description, temperature):
-    return History(location, date, description, temperature)
